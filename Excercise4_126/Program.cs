e5614fea004d79aed1545802acb7cdb06d4779a0
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-12-05 15:57:17 +0700

    Add array

--- Excercise4_126/Program.cs
+++ Excercise4_126/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Excercise4_126
 {
+    class Stack
+    {
+        char [] Fahrizal = new char [42];
+
+    }
     internal class Program
     {
         static void Main(string[] args)
